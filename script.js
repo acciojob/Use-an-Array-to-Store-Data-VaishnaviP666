e98ccdc31500abc22cecb85a91ee72471f2d243d
VaishnaviP666 <122201508+VaishnaviP666@users.noreply.github.com>
2023-04-18 23:01:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+let yourArray = [
+  { id: 1, name: "john", age: "18", profession: true },
+  { id: 2, name: "jack", age: "20", profession: false },
+  { id: 3, name: "karen", age: "19", profession: true" },
+	{ id: 4, name: "vaishu", age: "20", profession: true" },
+	  { id: 53, name: "bujji", age: "19", profession: false" },
+];
